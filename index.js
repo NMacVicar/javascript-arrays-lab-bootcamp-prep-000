25a37f4e6b2657847af101d836a7ccf85e7a7b05
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-14 15:45:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,28 @@ function destructivelyRemoveFirstKitten(name){
   kittens.shift(name)
 }
 
+function appendKitten(name){
+  var newKittens = [...kittens]
+  newKittens.push(name)
+  return newKittens
+}
+
+function prependKitten(name){
+  var newKittens = [...kittens]
+  newKittens.unshift(name)
+  return newKittens
+}
+
+function removeLastKitten(name){
+  var newKittens = [...kittens]
+  newKittens.pop(name)
+  return newKittens
+}
+
+function removeFistKitten(name){
+  var newKittens = [...kittens]
+  newKittens.shift(name)
+  return newKittens
+}
+
+
