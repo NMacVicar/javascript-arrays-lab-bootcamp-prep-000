808848c6dc9bb14a617ef430bf854cc5ce9aa232
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-14 15:42:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const app = "I don't do much."
 
 
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+}
+
